60ffbbc1d47dbc4edc866e2541800f143f49a235
huutamisme <hohuutam287@gmail.com>
2024-11-01 15:18:14 +0700

    Implement user info page

--- app/(profile)/ProfileLayout.tsx
+++ app/(profile)/ProfileLayout.tsx
@@ -1,39 +1,39 @@
 import { FaUserAlt, FaCalendarDay, FaHeart, FaSignOutAlt } from "react-icons/fa";
 import Link from "next/link";
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 
 const ProfileLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const pathName = usePathname();
+    const menuItems = [
+        { href: "/info", icon: <FaUserAlt size={25} />, label: "Thông tin cá nhân" },
+        { href: "/appointment", icon: <FaCalendarDay size={25} />, label: "Lịch hẹn" },
+        { href: "/favorite", icon: <FaHeart size={25} />, label: "Mentor yêu thích" },
+        { href: "#", icon: <FaSignOutAlt size={25} />, label: "Đăng xuất" }
+    ];
+
     return (
         <div className="flex py-5 p-10">
-
             {/* Menu */}
             <div className="flex flex-col w-1/5 items-start">
-                <Link href="/" className="text-5xl text-customBlue font-bold h-1/5">MENTOR</Link>
-                <ul className="menu rounded-box text-customBlue text-xl space-y-4 h-4/5">
-                    <li>
-                        <Link href="/profile/info">
-                            <FaUserAlt size={25} />
-                            Thông tin cá nhân
-                        </Link >
-                    </li>
-                    <li>
-                        <Link href="/profile/appointment">
-                            <FaCalendarDay size={25} />
-                            Lịch hẹn
-                        </Link >
-                    </li>
-                    <li>
-                        <Link href="/profile/favorite">
-                            <FaHeart size={25} />
-                            Mentor yêu thích
-                        </Link >
-                    </li>
-                    <li>
-                        <div>
-                            <FaSignOutAlt size={25} />
-                            Đăng xuất
-                        </div >
-                    </li>
+                <Link href="/" className="text-5xl text-customBlue font-bold h-1/6">MENTOR</Link>
+                <ul className="menu rounded-box text-customBlue text-xl space-y-4 h-5/6">
+                    {menuItems.map((item, index) => (
+                        <li key={index}>
+                            {item.href !== "#" ? (
+                                <Link href={item.href}
+                                    className={`${pathName === item.href ? 'bg-gray-300' : ''}`}>
+                                    {item.icon}
+                                    {item.label}
+                                </Link>
+                            ) : (
+                                <div>
+                                    {item.icon}
+                                    {item.label}
+                                </div>
+                            )}
+                        </li>
+                    ))}
                 </ul>
             </div>
 
@@ -44,4 +44,5 @@ const ProfileLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
         </div>
     );
 }
+
 export default ProfileLayout;
--- app/(profile)/info/page.tsx
+++ app/(profile)/info/page.tsx
@@ -1,39 +1,195 @@
 "use client";
 import ProfileLayout from "../ProfileLayout";
+import { useState } from "react";
+import { FaGraduationCap, FaBriefcase } from "react-icons/fa";
+
 
 const Info: React.FC = () => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [name, setName] = useState("Nguyễn Văn A");
+    const [phone, setPhone] = useState("0123456789");
+    const [email, setEmail] = useState("example@email.com");
+    const [edu, setEdu] = useState("Đại học Khoa Học Tự Nhiên thành phố Hồ Chí Minh");
+    const [major, setMajor] = useState("Kỹ thuật phần mềm");
+    const [certificate, setCertificate] = useState("");
+    const [field, setField] = useState("Phát triển giao diện web - FE developer");
+    const [shortTerm, setShortTerm] = useState("");
+    const [longTerm, setLongTerm] = useState("");
+
+    const toggleEdit = () => {
+        setIsEditing(!isEditing);
+    };
+
+    const updateInfo = () => {
+        // Logic cập nhật thông tin
+        toggleEdit();
+    };
+
     return (
         <ProfileLayout>
             <div className="mb-10">
                 <h1 className="text-4xl text-customBlue font-semibold">Thông tin cá nhân</h1>
             </div>
-            <div className="grid grid-cols-2 bg-gray-500 rounded-3xl p-10 bg-opacity-25">
-                <div className="bg-red-500 rounded-3xl mr-5">
+            <div className="grid grid-cols-2 space-x-6 bg-gray-500 rounded-3xl p-10 bg-opacity-25">
+                <div className="rounded-3xl">
                     <div className="card bg-base-100 w-full shadow-xl">
-                        <img
-                            src="/capy.jpg"
-                            alt="User Avatar" />
+                        <figure>
+                            <img src="/capy.jpg" alt="User Avatar" />
+                        </figure>
                         <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            {/* Form thông tin */}
+                            <div className="space-y-4 text-customBlue">
+                                <div>
+                                    <label className="font-bold">Họ tên:</label>
+                                    {isEditing ? (
+                                        <input
+                                            type="text"
+                                            value={name}
+                                            onChange={(e) => setName(e.target.value)}
+                                            className="input input-bordered w-full"
+                                        />
+                                    ) : (
+                                        <p>{name}</p>
+                                    )}
+                                </div>
+
+                                <div>
+                                    <label className="font-bold">Số điện thoại:</label>
+                                    {isEditing ? (
+                                        <input
+                                            type="text"
+                                            value={phone}
+                                            onChange={(e) => setPhone(e.target.value)}
+                                            className="input input-bordered w-full"
+                                        />
+                                    ) : (
+                                        <p>{phone}</p>
+                                    )}
+                                </div>
+
+                                <div>
+                                    <label className="font-bold">Email:</label>
+                                    {isEditing ? (
+                                        <input
+                                            type="email"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            className="input input-bordered w-full"
+                                        />
+                                    ) : (
+                                        <p>{email}</p>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="mt-4">
+                                <button
+                                    className="btn bg-customBlue text-white w-full hover:bg-secondary"
+                                    onClick={isEditing ? updateInfo : toggleEdit}
+                                >
+                                    {isEditing ? "Cập nhật" : "Chỉnh sửa"}
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col">
                     <div className="bg-white rounded-3xl mb-5 h-2/5 shadow-xl p-5">
-                        <p>Hello tôi là A</p>
-                        <p>Hello tôi là A</p>
-                        <p>Hello tôi là A</p>
-                        <p>Hello tôi là A</p>
+                        <div className="border-b-2 border-gray text-customBlue font-semibold flex items-center space-x-2">
+                            <FaGraduationCap size={40} />
+                            <p>Học vấn và Kinh nghiệm</p>
+                        </div>
+                        <div className="mt-4 space-y-4 text-customBlue overflow-y-auto max-h-64">
+                            <div>
+                                <label className="font-bold">Trường đại học:</label>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={edu}
+                                        onChange={(e) => setEdu(e.target.value)}
+                                        className="input input-bordered w-full"
+                                    />
+                                ) : (
+                                    <p>{edu}</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="font-bold">Chuyên ngành:</label>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={major}
+                                        onChange={(e) => setMajor(e.target.value)}
+                                        className="input input-bordered w-full"
+                                    />
+                                ) : (
+                                    <p>{major}</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="font-bold">Chứng chỉ:</label>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={certificate}
+                                        onChange={(e) => setCertificate(e.target.value)}
+                                        className="input input-bordered w-full"
+                                    />
+                                ) : (
+                                    <p>{certificate}</p>
+                                )}
+                            </div>
+                        </div>
                     </div>
                     <div className="bg-white rounded-3xl h-3/5 shadow-xl p-5">
-                        <p>Hello tôi là A</p>
-                        <p>Hello tôi là A</p>
-                        <p>Hello tôi là A</p>
-                        <p>Hello tôi là A</p>
+                        <div className="border-b-2 border-gray text-customBlue font-semibold flex items-center space-x-2">
+                            <FaBriefcase size={40} />
+                            <p>Mục tiêu nghề nghiệp</p>
+                        </div>
+                        <div className="mt-4 space-y-4 text-customBlue overflow-y-auto max-h-96">
+                            <div>
+                                <label className="font-bold">Lĩnh vực phát triển:</label>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={field}
+                                        onChange={(e) => setField(e.target.value)}
+                                        className="input input-bordered w-full"
+                                    />
+                                ) : (
+                                    <p>{field}</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="font-bold">Ngắn hạn:</label>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={shortTerm}
+                                        onChange={(e) => setShortTerm(e.target.value)}
+                                        className="input input-bordered w-full"
+                                    />
+                                ) : (
+                                    <p>{shortTerm}</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="font-bold">Dài hạn:</label>
+                                {isEditing ? (
+                                    <input
+                                        type="text"
+                                        value={longTerm}
+                                        onChange={(e) => setLongTerm(e.target.value)}
+                                        className="input input-bordered w-full"
+                                    />
+                                ) : (
+                                    <p>{longTerm}</p>
+                                )}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
